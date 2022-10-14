547d8c489d3cd42f910730f1328ae455bb7eeaaf
Sergey Kazarinov <kazarinov092@gmail.com>
2022-10-14 17:19:33 +0500

    feat(login): add secure: true for login

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -20,6 +20,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000,
         httpOnly: true,
+        secure: true,
       });
       res.send({ token });
     })
